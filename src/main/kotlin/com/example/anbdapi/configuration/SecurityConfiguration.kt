8131531e9549f7f5c585f95f0c1a20fe302845d0
jaebinGit <tnwoql327@gmail.com>
2025-02-04 02:28:57 +0900

    Feat : jwt 필터체인 구현 및 OAuth2로그인 추가

--- src/main/kotlin/com/example/anbdapi/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/anbdapi/configuration/SecurityConfiguration.kt
@@ -1,5 +1,9 @@
 package com.example.anbdapi.configuration
 
+import com.example.anbdapi.domain.dev.auth.global.JwtAuthenticationFilter
+import com.example.anbdapi.domain.dev.auth.global.OAuth2AuthenticationSuccessHandler
+import com.example.anbdapi.domain.dev.auth.service.CustomOAuth2UserServiceImpl
+import com.example.anbdapi.domain.dev.auth.utils.JwtUtil
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfiguration(
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val customOAuth2UserServiceImpl: CustomOAuth2UserServiceImpl,
+    private val oAuth2AuthenticationSuccessHandler: OAuth2AuthenticationSuccessHandler,
+    private val jwtUtil: JwtUtil
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -29,12 +37,15 @@ class SecurityConfiguration(
             }.sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }.authorizeHttpRequests {
-                it.anyRequest().permitAll() // 기호에 맞게 수정
-            }
-//            .addFilterBefore(
-//                jwtAuthenticationFilter(),
-//                UsernamePasswordAuthenticationFilter::class.java
-//            )
+                it.requestMatchers("/", "/swagger-ui/index.html", "/oauth2/**", "/api/v1/auth/refresh", "/api/v1/users/logout").permitAll()
+                    .anyRequest().authenticated()
+            }.oauth2Login {
+                it.userInfoEndpoint { userInfo -> userInfo.userService(customOAuth2UserServiceImpl) }
+                it.successHandler(oAuth2AuthenticationSuccessHandler)
+            }.addFilterBefore(
+                jwtAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
 
         return http.build()
     }
@@ -42,6 +53,8 @@ class SecurityConfiguration(
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
-//    @Bean
-//    fun jwtAuthenticationFilter(): JwtAuthenticationFilter = JwtAuthenticationFilter(objectMapper, tokenAuthService)
+    @Bean
+    fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
+        return JwtAuthenticationFilter(jwtUtil)
+    }
 }
