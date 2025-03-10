4e399b706ab3b712beb86a7749d324395710b6b7
jaebinGit <tnwoql327@gmail.com>
2025-03-10 18:59:56 +0900

    refactor : 파일 위치 변경 및 response 구조 변경, 생성자 구조 변경

--- src/main/kotlin/com/example/anbdapi/domain/user/dto/response/LoginResponse.kt
+++ src/main/kotlin/com/example/anbdapi/domain/user/dto/response/LoginResponse.kt
@@ -3,5 +3,5 @@ package com.example.anbdapi.domain.user.dto.response
 data class LoginResponse(
     val accessToken: String,
     val refreshToken: String,
-    val profileComplete: Int
+    val profileComplete: Boolean
 )
--- src/main/kotlin/com/example/anbdapi/support/response/UserResponseCode.kt
+++ src/main/kotlin/com/example/anbdapi/support/response/UserResponseCode.kt
@@ -3,5 +3,7 @@ package com.example.anbdapi.support.response
 class UserResponseCode {
     companion object {
         const val USER_01 = "User-001" // UserNotFoundException
+        const val USER_02 = "User-002" // UserImageUploadException
+        const val USER_03 = "User-003" // UserImageDeleteException
     }
 }
