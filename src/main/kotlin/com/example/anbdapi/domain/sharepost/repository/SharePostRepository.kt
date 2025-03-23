9b295aeb060328ce2a919d4a6d6262030e13e5a5
HwanGonJang <myggona@gmail.com>
2025-03-24 00:28:47 +0900

    fix: apply querydsl

--- src/main/kotlin/com/example/anbdapi/domain/sharepost/repository/SharePostRepository.kt
+++ src/main/kotlin/com/example/anbdapi/domain/sharepost/repository/SharePostRepository.kt
@@ -3,12 +3,9 @@ package com.example.anbdapi.domain.sharepost.repository
 import com.example.anbdapi.domain.sharepost.entity.SharePost
 import com.example.anbdapi.domain.user.entity.User
 import com.example.anbdapi.support.enums.ShareCategory
-import com.example.anbdapi.support.enums.ShareType
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -25,24 +22,4 @@ interface SharePostRepository : JpaRepository<SharePost, Long> {
 
     // 특정 사용자의 게시글을 페이징 조회
     fun findByUser(user: User, pageable: Pageable): Page<SharePost>
-
-    // TODO: QueryDSL로 리팩토링
-    @Query(
-        """
-        SELECT sp FROM SharePost sp 
-        WHERE sp.isSold = false
-        AND (sp.deletedAt IS NULL)
-        AND (:keyword IS NULL OR sp.title LIKE %:keyword% OR sp.content LIKE %:keyword%)
-        AND (sp.neighborhood = :location)
-        AND (:category IS NULL OR sp.category = :category)
-        AND sp.type = :type
-    """
-    )
-    fun findPosts(
-        @Param("keyword") keyword: String?,
-        @Param("location") location: String,
-        @Param("category") category: ShareCategory?,
-        @Param("type") type: ShareType?,
-        pageable: Pageable
-    ): Page<SharePost>
 }
